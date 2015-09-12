2f1af29aaa2e0193d6fb21cf5cf17dc828683b89
Shahril Amri <github@moefi.xyz>
2015-09-13 02:43:31 +0800

    gallery update
    added pre-mv album to gallery page replacing coming soon placeholder.
    - optimisations, fixes and customizations later

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -4,8 +4,12 @@
  *
  * galleria2 - Pre-UTPHAX'15
  * https://www.flickr.com/photos/134486812@N08/sets/72157656605521696
+ *
+ * galleria3 - Pre-UTPHAX'15 (MV)
+ * https://www.flickr.com/photos/134486812@N08/albums/72157658114228289
  */
- 
+
 // Initialize Galleria
 $('#galleria1').galleria({flickr: 'set:72157656677922975'});
 $('#galleria2').galleria({flickr: 'set:72157656605521696'});
+$('#galleria3').galleria({flickr: 'set:72157658114228289'});
