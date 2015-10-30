71899ee6ab0d9f1dc4fccbe281d94ef039e83c0e
Shahril Amri <github@moefi.xyz>
2015-10-30 19:48:23 +0800

    added gallery content
    also, replaced some slider

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -7,9 +7,13 @@
  *
  * galleria3 - Pre-UTPHAX'15 (MV)
  * https://www.flickr.com/photos/134486812@N08/albums/72157658114228289
+ *
+ * galleria4 - Dungun15
+ *
  */
 
 // Initialize Galleria
 $('#galleria1').galleria({flickr: 'set:72157656677922975'});
 $('#galleria2').galleria({flickr: 'set:72157656605521696'});
 $('#galleria3').galleria({flickr: 'set:72157658114228289'});
+$('#galleria4').galleria({flickr: 'set:72157660469686116'});
