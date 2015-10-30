ca432dac226f616d90021dfa017d600f4f10697b
Shahril Amri <github@moefi.xyz>
2015-10-30 09:28:39 +0800

    site cleanup
    - added gitignore
    - updated site robots
    - cleared default readme
    - removed orphan + unused content, layout and pages

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,4 +1,3 @@
-document.getElementById('pageDisabled').style.display='block';
 //jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
