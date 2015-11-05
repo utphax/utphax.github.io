b5c4dce9048a63c44b576c8e8cb67dfcde732f10
Shahril Amri <github@moefi.xyz>
2015-11-05 20:41:50 +0800

    updated gallery
    added PSP - gallery

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -2,13 +2,17 @@
  * galleria1 - UTPHAX'14
  * https://www.flickr.com/photos/134486812@N08/sets/72157656677922975
  *
- * galleria2 - Pre-UTPHAX'15
+ * galleria2 - Pre-UTPHAX'15 (UTP)
  * https://www.flickr.com/photos/134486812@N08/sets/72157656605521696
  *
- * galleria3 - Pre-UTPHAX'15 (MV)
+ * galleria3 - Pre-UTPHAX'15 MV (Royale Chulan)
  * https://www.flickr.com/photos/134486812@N08/albums/72157658114228289
  *
- * galleria4 - Dungun15
+ * galleria4 - Pre-UTPHAX'15 MV (PSP)
+ * https://www.flickr.com/photos/134486812@N08/albums/72157660110467449
+ *
+ * galleria5 - Training & Pre-UTPHAX'15 (Dungun)
+ * https://www.flickr.com/photos/134486812@N08/sets/72157660469686116
  *
  */
 
@@ -16,4 +20,5 @@
 $('#galleria1').galleria({flickr: 'set:72157656677922975'});
 $('#galleria2').galleria({flickr: 'set:72157656605521696'});
 $('#galleria3').galleria({flickr: 'set:72157658114228289'});
-$('#galleria4').galleria({flickr: 'set:72157660469686116'});
+$('#galleria4').galleria({flickr: 'set:72157660110467449'});
+$('#galleria5').galleria({flickr: 'set:72157660469686116'});
